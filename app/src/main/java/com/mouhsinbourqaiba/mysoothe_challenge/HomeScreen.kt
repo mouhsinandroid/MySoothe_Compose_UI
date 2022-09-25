e49ffdb6ca30a8ddc33c426e74aeac8f32b6020c
mouhsinandroid <moushin.bourqaiba.partner@decathlon.com>
2022-09-25 17:33:23 +0200

    adding bottom menu in home screen.

--- app/src/main/java/com/mouhsinbourqaiba/mysoothe_challenge/HomeScreen.kt
+++ app/src/main/java/com/mouhsinbourqaiba/mysoothe_challenge/HomeScreen.kt
@@ -2,11 +2,12 @@ package com.mouhsinbourqaiba.mysoothe_challenge
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,13 +16,38 @@ import com.mouhsinbourqaiba.mysoothe_challenge.ui.theme.MySootheChallengeTheme
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomNavigation()
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ },) {
+                Icon (
+                    Icons.Default.PlayArrow,
+                    contentDescription = null
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true
+    ) {
+        HomeScreenContent(
+           modifier =  Modifier.padding(it)
+        )
+    }
+}
+
+@Composable
+private fun HomeScreenContent(
+    modifier:Modifier =  Modifier
+) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         Column {
             Spacer(modifier = Modifier.height(72.dp))
-            
+
             MySootheTextField(
                 labelText = "Search",
                 leadingIcon = Icons.Default.Search,
@@ -32,7 +58,6 @@ fun HomeScreen() {
 
         }
     }
-
 }
 
 @Composable
@@ -87,6 +112,41 @@ private fun AlignYourMindSection() {
 
     CollectionRow(collections = alignYourMindCollections)
 
+}
+
+@Composable
+private fun BottomNavigation() {
+    BottomAppBar(
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        BottomNavigationItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    Icons.Default.Spa,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "HOME")
+            }
+        )
+
+        BottomNavigationItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    Icons.Default.AccountCircle ,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "PROFILE")
+            }
+        )
+    }
 }
 
 @Preview(
