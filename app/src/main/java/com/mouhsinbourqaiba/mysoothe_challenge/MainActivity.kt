464f6be4fc1d353d95cc57410aeb2f048e669919
mouhsinandroid <moushin.bourqaiba.partner@decathlon.com>
2022-09-25 15:25:44 +0200

    fix: clean code

--- app/src/main/java/com/mouhsinbourqaiba/mysoothe_challenge/MainActivity.kt
+++ app/src/main/java/com/mouhsinbourqaiba/mysoothe_challenge/MainActivity.kt
@@ -3,9 +3,6 @@ package com.mouhsinbourqaiba.mysoothe_challenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.mouhsinbourqaiba.mysoothe_challenge.ui.theme.MySootheChallengeTheme
